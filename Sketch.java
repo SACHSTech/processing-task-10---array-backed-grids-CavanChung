73a83473fe7cbbadc4bd4ab4414a90f6817ec000
Cavan Chung <75390863+CavanChung@users.noreply.github.com>
2022-06-20 01:14:21 +0000

    did level 2 and 3.1.5

--- Sketch.java
+++ Sketch.java
@@ -1,31 +1,96 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  // int variables declared
+  int intCellWidth = 20;
+  int intCellHeight = 20;
+  int intMargin = 5;
+  int intRowCount = 10;
+  int intColumnCount = 10;
+  int intScreenWidth = intCellWidth * intColumnCount + intMargin * (intColumnCount + 1);
+  int intScreenHeight = intCellHeight * intRowCount + intMargin * (intColumnCount + 1);
+
+  // 2d array
+  int[][] intGrid = new int[10][10];
+  
+  
+  // set screen width and height based on grid size using math
   public void settings() {
-    // put your size call here
-    size(500, 600);
+    size(intScreenWidth, intScreenHeight);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(255,255,255);
+    background(0);
+   
+    
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
+
+    // creates the grid
+    for (int intRow = 0; intRow < intRowCount; intRow++){
+      for (int intColumn = 0; intColumn < intColumnCount; intColumn++){
+        
+        if (intGrid[intRow][intColumn] == 1){
+          fill(0, 255, 0);
+        } 
+        else if (intGrid[intRow][intColumn] == 0){
+          fill(255);
+        }
+
+        rect(intMargin + intMargin * intColumn + intCellWidth * intColumn, intMargin + intMargin * intRow + intCellHeight * intRow, intCellWidth, intCellHeight);
+        
+
+      }
+    }
+    
   }
 
   public void mousePressed(){
+
+    int intGridRow = (int)(mouseY / (intMargin + intCellHeight));
+    int intGridColumn = (int)(mouseX / (intMargin + intCellWidth));
+
+    // changes square to green on mouse click
+    if (intGrid[intGridRow][intGridColumn] == 0) {
+      intGrid[intGridRow][intGridColumn] = 1;
+    }
+    else if (intGrid[intGridRow][intGridColumn] == 1){
+      intGrid[intGridRow][intGridColumn] = 0; 
+    }
+
+    // green square top
+    if (intGridRow > 0 && intGrid[intGridRow - 1][intGridColumn] == 0) {
+      intGrid[intGridRow - 1][intGridColumn] = 1;
+    }
+    else if (intGridRow > 0 && intGrid[intGridRow - 1][intGridColumn] == 1) {
+      intGrid[intGridRow - 1][intGridColumn] = 0;
+    }
+
+    // green square bottom
+    if (intGridRow > 0 && intGrid[intGridRow + 1][intGridColumn] == 0) {
+      intGrid[intGridRow + 1][intGridColumn] = 1;
+    }
+    else if (intGridRow > 0 && intGrid[intGridRow + 1][intGridColumn] == 1) {
+      intGrid[intGridRow + 1][intGridColumn] = 0;
+    }
+
+     // green square left
+    if (intGridRow > 0 && intGrid[intGridRow][intGridColumn - 1] == 0) {
+      intGrid[intGridRow][intGridColumn - 1] = 1;
+    }
+    else if (intGridRow > 0 && intGrid[intGridRow][intGridColumn - 1] == 1) {
+      intGrid[intGridRow][intGridColumn - 1] = 0;
+    }
+
+     // green square right
+    if (intGridRow > 0 && intGrid[intGridRow][intGridColumn + 1] == 0) {
+      intGrid[intGridRow][intGridColumn + 1] = 1;
+    }
+    else if (intGridRow > 0 && intGrid[intGridRow][intGridColumn + 1] == 1) {
+      intGrid[intGridRow][intGridColumn + 1] = 0;
+    }
     
   }
 }
